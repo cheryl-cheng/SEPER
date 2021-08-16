2b0c12ff40d48d198954dcaa9c2a951637a88589
Cheryl <cherylchengnz@gmail.com>
2021-08-16 23:45:56 +1200

    commit

--- frontend/src/pages/Submit-Article.js
+++ frontend/src/pages/Submit-Article.js
@@ -1,12 +1,13 @@
 import React from "react";
- 
+import SubmissionForm from "../components/SubmissionForm.js";
+
 const SubmitArticle = () =>  {
      return (
       <div>
         <h2>Submit Article</h2>
             <p>This will be a form to allow submitters to submit an article for possible inclusion in our SEPER repo.</p>
-            <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
-            <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
+            <SubmissionForm/>
+
         </div>
     );
 }
