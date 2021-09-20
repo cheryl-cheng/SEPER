e608e57a12f96fea2c8ae1c9e97f4d17c9d78a3a
Maya Munn <fyy7680@autuni.ac.nz>
2021-09-20 21:03:05 +1200

    Correctly directing user to frontend on deployment

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get("/", (req, res) =>
-  res.send(path.join(__dirname, "/frontend/public", "index.html"))
-);
+app.get("/", (req, res) => res.send("Hello world!"));
 
 const port = process.env.PORT || 8082;
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
