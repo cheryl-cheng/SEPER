a64d682920e1aa631000fbc3c5b8b59bb2da8993
Maya Munn <fyy7680@autuni.ac.nz>
2021-09-28 10:19:11 +1300

    Heroku issues potential fix

--- frontend/src/api/Article.js
+++ frontend/src/api/Article.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 import db from "../env.json";
+require("dotenv").config();
 
 export const submitArticleSuggestion = (articleData) => {
-  axios.post(db.dev + "/api/submit-suggestion", articleData);
+  if (process.env.NODE_ENV === "development") {
+    axios.post(db.dev + "/api/submit-suggestion", articleData);
+  } else {
+    axios.post(db.prod + "/api/submit-suggestion", articleData);
+  }
 };
