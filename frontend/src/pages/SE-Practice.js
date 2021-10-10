70fa93e6b3b42864fcf6deebad23474ca5b79900
Maya Munn <fyy7680@autuni.ac.nz>
2021-10-10 23:47:38 +1300

    Fixed wrongly merged code

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import Styles from "../components/tablestyle.js";
 import Table from "../components/evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
-import Dropdown from "../components/Dropdown.js";
 import { getArticles } from "../api/Article.js";
 
 const SEPractice = () => {
@@ -22,7 +21,7 @@ const SEPractice = () => {
   return (
     <div>
       <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-      <Dropdown />
+      {/* <Dropdown /> */}
       <Styles>
         <Table data={articles} columns={tablecolumns} />
       </Styles>
